7bcef688bdd009b3656847504e5e7c490032bd65
jiweiwang <jiwei_it@163.com>
2022-08-18 17:33:38 +0800

    "collections模块介绍/数据类型|抽象基类"

---
+++ chapter01/__init__.py
@@ -0,0 +1,6 @@
+"""
+    collections 模块介绍
+    1.  collections源码查看
+    2.  常用的数据类型
+    3.  抽象基类
+"""
---
+++ chapter01/demo01.py
@@ -0,0 +1,8 @@
+# encoding: utf-8
+# author: nextHeartbeat
+# software: pycharm
+# file: demo01.py
+# time: 2022/8/18 16:38
+
+from collections import *  # 查看源码, 数据结构
+from collections.abc import *  # 抽象基类 (interface)
